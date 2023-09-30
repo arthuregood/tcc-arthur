95fa4429e833a0bce8e6d5dced17ad500b356729
arthuregood <arthuregood@protonmail.com>
2023-09-30 13:15:16 -0300

    made the tcc-arthur folder as the main ROS folder

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/arthuregood/tcc-arthur/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
+export PWD='/home/arthuregood/tcc-arthur/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/arthuregood/tcc-arthur/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/arthuregood/tcc-arthur/src:/opt/ros/noetic/share'
